12a07c438de2d7f575e2f1825fdf2355c53db28e
Alin Radu <alinradu07@yahoo.com>
2024-09-18 13:12:02 +0300

    basic-vite-react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import reactLogo from "./assets/react.svg";
+function App() {
+  return (
+    <>
+      <h1>
+        <span>
+          <img src={reactLogo} alt="React Logo" />
+        </span>
+        IT-TEST
+      </h1>
+      <p>Content</p>
+      <button>START</button>
+    </>
+  );
+}
+
+export default App;
